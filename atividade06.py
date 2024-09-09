514ed76cb0f3992875f003288d8c24438a6535e6
Samylaneres <samyaneres13@gmail.com>
2024-09-09 09:03:33 -0300

    dois nomes

---
+++ atividade06.py
@@ -0,0 +1,4 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+nome=str(input("digite seu nome"))
+sobrenome=str(input("digite seu sobrenome"))
+nomecom=print (f"seu nome é {nome} {sobrenome}")
